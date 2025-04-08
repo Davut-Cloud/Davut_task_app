a9c8e2da767b0a2f2afdb668e514b40cf3647a7d
Davut <davesizzle@googlemail.com>
2025-04-08 13:52:26 +0200

    solved task 4524/4523

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,34 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String name = '';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(name),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              name = 'Davut Coskun';
+            });
+          },
+          child: const Text('Name anzeigen'),
+        ),
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -2,8 +2,53 @@ import 'package:flutter/material.dart';
 
 class S4524 extends StatelessWidget {
   const S4524({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = '';
+  String buttonText = 'Name anzeigen';
+
+  final String meinName = 'Davut Coskun';
+
+  void toggleName() {
+    setState(() {
+      if (name.isEmpty) {
+        name = meinName;
+        buttonText = 'Name verstecken';
+      } else {
+        name = '';
+        buttonText = 'Name anzeigen';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          name,
+          style: const TextStyle(fontSize: 24),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: toggleName,
+          child: Text(buttonText),
+        ),
+      ],
+    );
   }
 }
